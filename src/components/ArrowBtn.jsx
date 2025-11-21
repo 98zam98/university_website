d7deb9694083412378df342d64c9bcff46624f06
Zyad Mackawy <zyad1998mackawy@gmail.com>
2025-11-21 08:44:16 +0200

    added more to the animations

--- src/components/ArrowBtn.jsx
+++ src/components/ArrowBtn.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ArrowBtn = () => {
     return (
         <button className=" active:scale-95 transition-transform
-            w-12 h-12 flex items-center justify-center rounded-full border border-1 text-1 hover:bg-4 active:bg-4 hover:text-0 
+            w-12 h-12 flex items-center justify-center rounded-full 
+            border border-1 text-1 hover:bg-4 active:bg-4 hover:text-0 
+            hover:scale-115
         ">
             →
         </button>
--- src/components/MainBtn.jsx
+++ src/components/MainBtn.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const MainBtn = ({ text = "" }) => {
     return (
         <button className=" active:scale-95 transition-transform
-            px-6 py-2 bg-4 hover:bg-4/80  active:bg-4/20 text-white rounded-full  
+            px-6 py-2 bg-4 hover:bg-4/80  active:bg-4/20 text-white rounded-full 
+            hover:-translate-y-1 hover:shadow-lg  duration-200 
+            hover:scale-105
+            hover:-rotate-3
+            hover:rotate-y-30
+ 
         ">
             {text}
         </button>
