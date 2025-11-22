84c6de75b1bdcd8e247c109e266d55f302f2d921
Zyad Mackawy <zyad1998mackawy@gmail.com>
2025-11-22 21:05:26 +0200

    Updates

---
+++ assets/FolderShaped-BHPJEZOU.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-DoHHYGbY.js";const l="primaryblue",n=({children:a,bgColor:s=null,borderColor:e=null})=>r.jsxs("div",{className:`\r
+                 w-full h-full  flex flex-col duration-500\r
+                 hover:scale-95  hover:rotate-x-10  hover:-rotate-y-5 rounded-4xl\r
+                 hover:-translate-y-5 \r
+            `,children:[r.jsx("div",{className:` w-1/3  bg-${e??l} rounded-2xl aspect-square `,style:{marginBottom:"-25%"}}),r.jsx("div",{className:` bg-cover bg-center rounded-4xl bg-${l}  bg-${s??e??l}  w-full flex-1 border-primaryblue  ${e?` border-${e}    border-6 `:""}  `,children:a})]});export{n as F};
---
+++ assets/NameInTopLeft-rQhvBJOp.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DoHHYGbY.js";import{N as m}from"./NameArr-DAYEzg1l.js";const e=({text:r="",textColor:o})=>t.jsx(m,{arr:[r],textColor:o});export{e as N};
